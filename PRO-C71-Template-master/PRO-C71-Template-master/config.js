511c4bb0e6d806e7bbd205683d74da11c822ae15
K_FuRY <81895448+KFuRYE@users.noreply.github.com>
2022-03-22 17:31:05 +0530

    Delete config.js

--- PRO-C71-Template-master/PRO-C71-Template-master/config.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase";
-require("@firebase/firestore");
-
-//Paste your firebaseConfig here
-
-//
-firebase.initializeApp(firebaseConfig);
-
-export default firebase.firestore();
